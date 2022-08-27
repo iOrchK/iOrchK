1713666ea0a3db6716cb1b864f751543901bb49a
Jorge Chable <jorge.chable@roomie-it.org>
2022-08-27 12:52:06 -0500

    first deploy

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
-- 👋 Hi, I’m @iOrchK
-- 👀 I’m interested in Software Development
-- 🌱 I’m currently learning about Google Cloud Platform
-- 💞️ I’m looking for a Stable and Home Office work
-- 📫 How to reach me: yahicimosclick9193@gmail.com
-
-<!---
-iOrchK/iOrchK is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+# react-tesseract-ui
+Simple tesseract ui example
---
+++ precache-manifest.388e484f9f4e3c02700db8e25fecf491.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b11d2971a2e40d8268fa31735e5533b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aa5d23980162dcdf8ab",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1f84f434edda4cb3d4c2",
+    "url": "/static/js/2.64eefcd2.chunk.js"
+  },
+  {
+    "revision": "2aa5d23980162dcdf8ab",
+    "url": "/static/js/main.72f785ac.chunk.js"
+  },
+  {
+    "revision": "624deeba909b1bded280",
+    "url": "/static/js/runtime-main.9577a950.js"
+  }
+]);
